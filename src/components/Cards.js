857ab11efc45a5970ba0eda5a7760b49c3a163c4
unknown <quisbertvarinia@gmail.com>
2021-07-07 20:59:14 -0300

    files updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,13 +11,13 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='https://i.pinimg.com/originals/44/f0/02/44f002166db0c224c90703f18a659dae.gif'
-              text='Busca la profesión en la que te encuentras interesado y descubre más sobre ella.'
+              text='Busca y descubre más sobre la profesión en la que te encuentras interesado.'
               label='Profesiones'
               path='/profesiones'
             />
             <CardItem
               src='https://i.pinimg.com/originals/f5/27/0a/f5270acbc4b98112fcd520d2eea023de.gif'
-              text='Haz el test para descubrir la profesión más adecuada para vos.'
+              text='Haz el test para descubrir la profesión más adecuada para tí.'
               label='Test de vocación profesional'
               path='/planesEstudio'
             />
