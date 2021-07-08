8c06df1e488f84058880bb7e55b57c5fc96e13cd
unknown <quisbertvarinia@gmail.com>
2021-07-07 21:44:04 -0300

    path video file edited

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src='../videos/video-1.mp4' autoPlay loop muted />
       <h1>Tu futuro está en tus manos</h1>
       <p>¿Estás listo?</p>
       <div className='hero-btns'>
