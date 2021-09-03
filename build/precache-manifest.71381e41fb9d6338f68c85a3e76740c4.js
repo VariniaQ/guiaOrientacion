a8fcea0c82bcc1c0075a1cb7316fc234759789ce
unknown <quisbertvarinia@gmail.com>
2021-09-02 21:26:45 -0300

    package.json edited

---
+++ build/precache-manifest.71381e41fb9d6338f68c85a3e76740c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3911dcd36fbc241fa56b03ee0ff9897a",
+    "url": "/guiaOrientaci%C3%B3n/index.html"
+  },
+  {
+    "revision": "27aa936adba46076a6cf",
+    "url": "/guiaOrientaci%C3%B3n/static/css/main.1edc44bc.chunk.css"
+  },
+  {
+    "revision": "299a5ff9acbb65ec2665",
+    "url": "/guiaOrientaci%C3%B3n/static/js/2.8e422992.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/guiaOrientaci%C3%B3n/static/js/2.8e422992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27aa936adba46076a6cf",
+    "url": "/guiaOrientaci%C3%B3n/static/js/main.cadfef13.chunk.js"
+  },
+  {
+    "revision": "cd5f6f9507898150a606",
+    "url": "/guiaOrientaci%C3%B3n/static/js/runtime-main.a87e132f.js"
+  },
+  {
+    "revision": "91cd4ff4c3736e0dcdc481088d9b3252",
+    "url": "/guiaOrientaci%C3%B3n/static/media/electronica2.91cd4ff4.jpg"
+  },
+  {
+    "revision": "953601335a6287b00ed3849232f03aa6",
+    "url": "/guiaOrientaci%C3%B3n/static/media/hardware.95360133.jpg"
+  },
+  {
+    "revision": "1125c8ff4efbba538d2ee0c7b0cdd994",
+    "url": "/guiaOrientaci%C3%B3n/static/media/img-2.1125c8ff.jpg"
+  },
+  {
+    "revision": "37ff962142baa74f1b10bf91b25364d9",
+    "url": "/guiaOrientaci%C3%B3n/static/media/logo20.37ff9621.png"
+  },
+  {
+    "revision": "4c18020e11983dc30a39344997ce3450",
+    "url": "/guiaOrientaci%C3%B3n/static/media/sistemas.4c18020e.jpg"
+  },
+  {
+    "revision": "229f5279475d67e3cf902f7ab5cd818b",
+    "url": "/guiaOrientaci%C3%B3n/static/media/video-1.229f5279.mp4"
+  }
+]);
