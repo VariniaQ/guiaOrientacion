b0e4966beff6eff42e44967fabc3590d337cdb0d
VariniaQ <quisbertvarinia@gmail.com>
2021-09-14 11:38:35 -0300

    home video edited

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,18 +2,16 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import video from '../images/video-1.mp4';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src={video} autoPlay loop muted />
       <h1>Tu futuro está en tus manos</h1>
       <p>¿Estás listo?</p>
       <div className='hero-btns'>
         <Button
           className='btns'
-          buttonStyle='btn--outline'
+          buttonStyle='btn--home'
           buttonSize='btn--large'
         >
           COMIENZA
