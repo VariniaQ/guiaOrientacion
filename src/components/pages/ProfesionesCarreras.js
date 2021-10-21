79ecc864232953689d35c5289266b0a880916584
VariniaQ <quisbertvarinia@gmail.com>
2021-10-20 23:00:38 -0300

    updating pages

--- src/components/pages/ProfesionesCarreras.js
+++ src/components/pages/ProfesionesCarreras.js
@@ -7,15 +7,23 @@ function ProfesionesCarreras() {
     return (
          <>
             <div className="carreras">
-               <div className="contenedor-carrera-img">
-                  <img src={sistemas} alt="sistemas"/>
-               </div>
+            
+
                {/*titulo carrera*/}
-               <h1>Ingeniería en Sistemas</h1>
-               {/*descripcion carrera*/}
+               {/*<h1 className="titulo">Ingeniería en Sistemas</h1>*/}
+
+               {/* Imagen de la carrera
+               <div className="contenedor-carrera-img">
+                  <img className="carrera-img" src={sistemas} alt="sistemas"/>
+               </div>
+               */}
+
+               {/*descripcion de la carrera
                <p className="descripcion-carrera">Se encarga de resolver problemas relacionados al control de procesos industriales, 
-               sistemas electrónicos de potencia, instrumentación y control, y la transformación eléctrica.</p>
-               {/*lista de universidades
+               sistemas electrónicos de potencia, instrumentación y control, y la transformación eléctrica.</p>*/}
+               
+               
+               {/* lista de universidades (acepta linkeado)
                <div className="universidades-logos">
                   <a className="logo" href="https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=3&id_seleccion=67" rel="noreferrer">
                      <img src={utn} alt="UTN"/>
@@ -28,10 +36,15 @@ function ProfesionesCarreras() {
                   </a>
                </div>
                   */}     
-               {/*<Link to='/' className='boton-contenido'>
+
+
+               {/* Botón "Contenido" (aún no terminado)
+               <Link to='/' className='boton-contenido'>
                   <img src={logo} className="adjust-icon" alt="logo-busqueda"></img>Contenido
                </Link>*/}
-               <IconCards />
+
+               {/*Universidades cards*/}
+               {/*<IconCards />*/}
             </div>
          </>
     );
